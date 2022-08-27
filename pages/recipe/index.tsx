c4593a895974e696f9d6a0201112c0b26d03021b
atsumaru1377 <62948930+atsumaru1377@users.noreply.github.com>
2022-08-27 14:55:55 +0900

    modify img

--- pages/recipe/index.tsx
+++ pages/recipe/index.tsx
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
               display:"flex",
               maxHeight:"360px"
             }}>
-            <img src="../../public/static/images/sample_thumb.png" alt = "thumbnail" height="368" style={{objectFit:"cover"}} />
+            <img src="/static/images/sample_thumb.png" alt = "thumbnail" height="368" style={{objectFit:"cover"}} />
             <div style={{
               width:"calc(80% - 320px)",
               padding:"16px"
