41100eb5555c50ac08c65ae2ec6475286ad411d5
横井総太朗 <alphacomplex.104729@gmail.com>
2022-08-30 00:13:18 +0900

    top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,11 @@ import Tabs from "./components/Tabs"
 import SearchBar from "./components/searchBar"
 import Grid from "@mui/material/Grid"
 import Footer from "./components/footer"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import Divider from "@mui/material/Divider"
+import RepoCard from "./components/tukurepoCard"
+import {CommuCardType,DesignCardType,RecipeCardType} from "../plugins/type"
 
 const boxStyle:{[key:string]:string} = {
   paddingLeft:"256px",
@@ -17,16 +22,8 @@ const boxStyle:{[key:string]:string} = {
   top:"0",
   width:'calc(100% - 280px)',
 }
-type CardData = {
-  src:string;
-  title:string;
-  tool:string;
-  feature:string;
-  theme1:string;
-  theme2:string;
-  theme3:string;
-};
-const datas : CardData[] = [
+
+const datas : RecipeCardType[] = [
     { src:"/static/images/recipe_thumb/sample_1.png",
     title:"illustratorで柔らかいグラフィックを制作",
     tool:"illustrator",
@@ -82,35 +79,15 @@ const datas : CardData[] = [
     feature:"グラフィック",
     theme1:"3D",
     theme2:"CG",
-    theme3:"可愛い"},
-    { src:"/static/images/recipe_thumb/sample_3.png",
-    title:"blenderで氷が落ちる動画を作る",
-    tool:"blender",
-    feature:"モデリング",
-    theme1:"ゲーム",
-    theme2:"3D",
-    theme3:"CG"},
-    { src:"/static/images/recipe_thumb/sample_4.png",
-    title:"After Effectsで波打つモーション制作",
-    tool:"After Effects",
-    feature:"モーション",
-    theme1:"HP",
-    theme2:"LP",
-    theme3:"動画"},
-    { src:"/static/images/recipe_thumb/sample_5.png",
-    title:"Cinema4DでサイバーなCGを作る",
-    tool:"Cineme4D",
-    feature:"モデリング",
-    theme1:"3D",
-    theme2:"CG",
-    theme3:"動画"},
-    { src:"/static/images/recipe_thumb/sample_6.png",
-    title:"Photoshopでエモーショナルな夜景に加工",
-    tool:"PhotoShop",
-    feature:"加工",
-    theme1:"Web",
-    theme2:"LP",
-    theme3:"写真"}
+    theme3:"可愛い"}
+]
+const design : DesignCardType[] = [
+  {src:"/static/images/designReport/tuku_1.png"},
+  {src:"/static/images/designReport/tuku_2.png"},
+  {src:"/static/images/designReport/tuku_3.png"},
+  {src:"/static/images/designReport/tuku_4.png"},
+  {src:"/static/images/designReport/tuku_5.png"},
+  {src:"/static/images/designReport/tuku_6.png"},
 ]
 const Home: NextPage = () => {
   return (
@@ -125,8 +102,15 @@ const Home: NextPage = () => {
             <div style={{maxWidth:"1560px",margin:"0 auto",padding:"0 88px"}}>
             <SearchBar/>
             <Tabs/>
+            <Typography variant = "h2" sx={{
+                fontSize:"1.5rem",
+                fontWeight:"500",
+                mt:"32px",
+                mb:"16px",
+                ml:"16px"
+            }}>みんなのレシピ</Typography>
             <Grid container>
-            {datas.map((data: CardData,index:number) => {
+            {datas.map((data: RecipeCardType,index:number) => {
             return (
               <Grid item md= {12} lg={6} xl={4} key={index}>
                 <Card 
@@ -142,8 +126,33 @@ const Home: NextPage = () => {
               );
             })}
             </Grid>
+            <div style={{display:"flex",justifyContent:"center",marginBottom:"16px"}}>
+            <Button href="/recipe">もっと見る</Button>
             </div>
-
+            <Divider sx={{my:"16px"}}></Divider>
+            <Typography variant = "h2" sx={{
+                fontSize:"1.5rem",
+                fontWeight:"500",
+                mt:"32px",
+                mb:"16px",
+                ml:"16px"
+            }}>みんなのでざレポ</Typography>
+             <Grid container>
+              {design.map((data: DesignCardType,index:number) => {
+              return (
+                <Grid item xs={12} md= {12} lg={6} xl={4} key={index}>
+                  <RepoCard 
+                  src = {data.src}
+                  />
+                </Grid>
+                );
+              })}
+            </Grid>
+            <div style={{display:"flex",justifyContent:"center",marginBottom:"16px"}}>
+            <Button href="/recipe">もっと見る</Button>
+            </div>
+            <Footer></Footer>
+            </div>
         </Box>
       </Box>
     </>
