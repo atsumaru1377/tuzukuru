9f5e4e45ee0a78e8ea1e5f0e2fa79d527893b114
atsumaru1377 <62948930+atsumaru1377@users.noreply.github.com>
2022-08-30 23:28:53 +0900

    last commit?

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -124,7 +124,7 @@ const Header = () => {
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton role="button" onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
